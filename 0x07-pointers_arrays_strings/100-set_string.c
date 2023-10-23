2f8cf8f8e14aaa0f3ba560418340d17c491f9f0d
Muslimah <sarumiatinuke2020@gmail.com>
2023-10-23 13:22:19 -0700

    gooooo

--- 0x07-pointers_arrays_strings/100-set_string.c
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -1,13 +1,6 @@
-#include "main.h"
-
-/**
- * set_string - sets the value of a pointer to a char
- * @s: content is to be copied
- *@to: string
- *Return: void
- */
+#include <stdio.h>
 
 void set_string(char **s, char *to)
 {
-	*s = to;
+    *s = to;
 }
