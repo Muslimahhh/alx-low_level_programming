eced4e54a14f7e9263c3e2bb9b310be803f0a511
Muslimah <sarumiatinuke2020@gmail.com>
2023-10-24 06:33:40 -0700

    jury

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -1,43 +1,12 @@
 #include "main.h"
 
 /**
- * print_number - prints out a number
- *@n: number to be printed
+ * print_number - Prints a square followed by a newline
+ * @size: size argument
+ * Return: void
  */
 
-void print_number(int n)
+void print_triangle(int n)
 {
-int  y;
-
-if (n > 9 && n < 19)
-{
-while (n > 9)
-{
-n = n % 10;
-}
-_putchar(1 + '0');
-_putchar(n + '0');
-}
-else if (n < 0)
-{
-y = n * -1;
-_putchar('-');
-if (y > 9)
-{
-while (y > 9)
-{
-n = y % 10;
-}
-_putchar(9 + '0');
-_putchar(n + '0');
-}
-else
-{
-_putchar(y + '0');
-}
-}
-else
-{
-_putchar(n + '0');
-}
+	_putchar('\n');
 }
