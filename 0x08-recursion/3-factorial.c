472115a8aa8ef6d4a80d32d37b7f9745cc11801d
Sarumi Atinuke Muslimah <81876671+Muslimahhh@users.noreply.github.com>
2024-07-11 13:26:07 +0100

    Update 3-factorial.c

--- 0x08-recursion/3-factorial.c
+++ 0x08-recursion/3-factorial.c
@@ -3,7 +3,7 @@
   * factorial - calculates the factorial of a given number
   * @n: - first integer argument
   * Return: the factorial pof a  number
-  */
+  */ 
 int factorial(int n)
 {
 	if (n < 0)
