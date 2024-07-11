232fac3b1db491611b3196d58b2d356e695f3e0e
Sarumi Atinuke Muslimah <81876671+Muslimahhh@users.noreply.github.com>
2024-07-11 13:30:51 +0100

    i think this would work

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -1,51 +1,45 @@
-#include "main.h"
+#include <main.h>
 
-/**
- * is_palindrome - returns 1 if a string is a palindrome and 0 if not
- *
- * @s: string
- * Return: int
- */
+// Function prototypes
+int is_palindrome(char *s);
+int _strlen_recursion(char *s);
+int palindrome_check(char *s, int len, int i);
+
+// Main function for testing
+int main(void)
+{
+    char s1[] = "racecar";
+    char s2[] = "hello";
+    char s3[] = "";
+    
+    printf("%s is %sa palindrome\n", s1, is_palindrome(s1) ? "" : "not ");
+    printf("%s is %sa palindrome\n", s2, is_palindrome(s2) ? "" : "not ");
+    printf("%s is %sa palindrome\n", s3, is_palindrome(s3) ? "" : "not ");
+    
+    return 0;
+}
+
+// Function definitions
 int is_palindrome(char *s)
 {
-	int len = _strlen_recursion(s), i = 0;
-
-	if (len == 0)
-		return (1);
-	return (palindrome_check(s, len - 1, i));
+    int len = _strlen_recursion(s);
+    if (len == 0)
+        return 1;
+    return palindrome_check(s, len - 1, 0);
 }
 
-/**
- * _strlen_recursion - returns the length of a string
- *
- * @s: string
- * Return: int
- */
 int _strlen_recursion(char *s)
 {
-	int len;
-
-	if (*s == '\0')
-		return (0);
-	len = _strlen_recursion(s + 1);
-	return (len + 1);
+    if (*s == '\0')
+        return 0;
+    return 1 + _strlen_recursion(s + 1);
 }
 
-/**
- * palindrome_check - returns 1 if palindrome and 0 if not
- *
- * @s: string
- * @len: length of string
- * @i: counter variable
- * Return: int
- */ 
 int palindrome_check(char *s, int len, int i)
 {
-	if (len == i)
-		return (1);
-	else if (s[len] != s[i])
-		return (0);
-	if (i < len)
-		return (palindrome_check(s, --len, ++i));
-	return (1);
+    if (i >= len)
+        return 1;
+    if (s[len] != s[i])
+        return 0;
+    return palindrome_check(s, len - 1, i + 1);
 }
