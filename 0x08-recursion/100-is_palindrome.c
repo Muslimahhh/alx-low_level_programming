666b40d16fdc266cc4dc79a6f7d1679ace3db824
Sarumi Atinuke Muslimah <81876671+Muslimahhh@users.noreply.github.com>
2024-07-11 13:24:02 +0100

    Update 100-is_palindrome.c

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -1,28 +1,51 @@
 #include "main.h"
+
 /**
-  * _is_divide - checks if a number is a prime
-  * @x: inter arg 1
-  * @y: int arg 2
-  * Return: prime number of int
-  */
-int _is_divide(int x, int y)
+ * is_palindrome - returns 1 if a string is a palindrome and 0 if not
+ *
+ * @s: string
+ * Return: int
+ */
+int is_palindrome(char *s)
 {
-	if ((x % y) == 0)
-		return (0);
-	if (y == (x / 2))
+	int len = _strlen_recursion(s), i = 0;
+
+	if (len == 0)
 		return (1);
-	return (_is_divide(x, y + 1));
+	return (palindrome_check(s, len - 1, i));
 }
+
 /**
-  * is_prime_number - checks for prime number of an int
-  * @n: Integer argument
-  * Return: inter prime number
-  */
-int is_prime_number(int n)
+ * _strlen_recursion - returns the length of a string
+ *
+ * @s: string
+ * Return: int
+ */
+int _strlen_recursion(char *s)
 {
-	if (n <= 1)
+	int len;
+
+	if (*s == '\0')
 		return (0);
-	if (n > 1 && n < 4)
+	len = _strlen_recursion(s + 1);
+	return (len + 1);
+}
+
+/**
+ * palindrome_check - returns 1 if palindrome and 0 if not
+ *
+ * @s: string
+ * @len: length of string
+ * @i: counter variable
+ * Return: int
+ */
+int palindrome_check(char *s, int len, int i)
+{
+	if (len == i)
 		return (1);
-	return (_is_divide(n, 2));
+	else if (s[len] != s[i])
+		return (0);
+	if (i < len)
+		return (palindrome_check(s, --len, ++i));
+	return (1);
 }
