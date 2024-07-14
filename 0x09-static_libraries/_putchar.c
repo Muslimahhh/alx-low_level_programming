414ac1c7ff6e701a0249e6447ee2a8fb963af62c
Sarumi Atinuke Muslimah <81876671+Muslimahhh@users.noreply.github.com>
2024-07-14 22:11:20 +0100

    Update _putchar.c

--- 0x09-static_libraries/_putchar.c
+++ 0x09-static_libraries/_putchar.c
@@ -1,5 +1,21 @@
 #include "main.h"
 
-int _putchar(char c)
+/**
+ * main-entry point
+ *
+ * Description: This program prints _putchar followed by a new line
+ * Return: Always 0 (sucess)
+*/
+int main(void)
 {
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+	return (0);
 }
