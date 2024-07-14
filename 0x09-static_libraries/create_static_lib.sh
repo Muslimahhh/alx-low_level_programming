6db26b1ad7e418e33cede7e48940413738e6a214
Sarumi Atinuke Muslimah <81876671+Muslimahhh@users.noreply.github.com>
2024-07-14 21:33:22 +0100

    Update create_static_lib.sh

--- 0x09-static_libraries/create_static_lib.sh
+++ 0x09-static_libraries/create_static_lib.sh
@@ -1,16 +1,4 @@
 #!/bin/bash
-
-# Compile all .c files into .o files
-gcc -c *.c
-
-# Create the static library from all the .o files
+gcc -Wall -pedantic -Werror -Wextra -c *.c
 ar -rc liball.a *.o
-
-# Index the library (optional, but good practice)
 ranlib liball.a
-
-# Clean up the .o files (optional, but keeps the directory clean)
-rm -f *.o
-
-# Print a success message
-echo "Static library liball.a created successfully."
